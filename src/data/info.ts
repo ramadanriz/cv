637887295f1766345b887fc0ca03422f9e14cca8
ramadanriz <zerorider96@gmail.com>
2024-01-18 07:14:33 +0700

    initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,78 @@
+export const info = {
+  baseUrl: "https://ramadanriz.netlify.app",
+  name: "Rizky Ramadhani",
+  jobDescription: "Web Developer",
+  about: `Hello, world! My name is Rizky Ramadhani. I'm a Junior Web Developer, and Computer Science Student. I live in Jombang, East Java, Indonesia. I have experience as a Junior Web Developer. I have an interest in a career as a Full Stack Developer. I have learned a lot of new technologies in the past few years by myself on the Internet.
+  `,
+
+  experience: [
+    {
+      name: "Dicoding (Studi Independen Bersertifikat Kampus Merdeka)",
+      location: "Bandung, East Java",
+      startDate: "February 2022",
+      endDate: "August 2022",
+      description: [
+        "- Building layouts and the structure of web pages.",
+        "- Integrating with the back-end to fetch data from the server.",
+        "- Developing and implementing business logic and data processing algorithms.",
+        "- Providing an API to communicate with the front-end.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Universitas Hasyim Asy'ari",
+      location: "Tebuireng, Jombang",
+      startDate: "2019",
+      endDate: "2023",
+      description: [
+        "Computer Science",
+      ],
+    },
+    {
+      name: "SMK Negeri 3 Jombang",
+      location: "Jombang",
+      startDate: "2015",
+      endDate: "2018",
+      description: ["Computer Network Engineering"],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/ramadanriz",
+    email: "mailto:ramadhanriz57@gmail.com",
+    linkedin: "https://www.linkedin.com/in/ramadanriz",
+  },
+
+  projects: [
+    {
+      title: "do.it",
+      isFeatured: true,
+      thumbnail: "/assets/images/doit.png",
+      githubUrl: "https://github.com/ramadanriz/do-it",
+      liveUrl: "https://doit-sib-project.web.app/",
+    },
+    {
+      title: "react notes",
+      isFeatured: true,
+      thumbnail: "/assets/images/note.png",
+      githubUrl: "https://github.com/ramadanriz/SPA-React-Notes-v2",
+      liveUrl: "https://spa-react-notes-v2.vercel.app/",
+    },
+    {
+      title: "goalgrid",
+      isFeatured: true,
+      thumbnail: "/assets/images/goal.png",
+      githubUrl: "https://github.com/ramadanriz/goal-grid",
+      liveUrl: "https://goalgrid.netlify.app/",
+    },
+    {
+      title: "sholat ngaji",
+      isFeatured: true,
+      thumbnail: "/assets/images/ngaji.png",
+      githubUrl: "https://github.com/ramadanriz/sholat-ngaji",
+      liveUrl: "https://sholat-ngaji.vercel.app/",
+    },
+  ],
+};
